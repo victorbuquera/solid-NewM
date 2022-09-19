5aa292f9390c65bce20806548ad160e600baae00
Victor <victorbuquera@gmail.com>
2022-09-19 20:17:20 -0300

    Melhorando exemplos

--- 5 - D.php
+++ 5 - D.php
@@ -1,20 +1,53 @@
 <?php
 
-interface Funcionario {
-    public function Trabalhar();
+interface DBConnectionInterface {
+    public function connect();
 }
 
-class Programador implements Funcionario{
-    public function Trabalhar()
+class MySQLConnection implements DBConnectionInterface{
+    /**
+     * @throws ErrorException
+     */
+    public function connect(): PDO
     {
-        return 'Codando';
+        try {
+            return new PDO('mysql:host=localhost; dbname=mysql','root','');
+        }catch (PDOException|Exception $e){
+            throw new ErrorException("Erro na conexão com o MySQL");
+        }
     }
 }
 
-class Tester implements Funcionario{
-    
-    public function Trabalhar()
+class PostGreeConnection implements DBConnectionInterface{
+    /**
+     * @throws ErrorException
+     */
+    public function connect(): PDO
     {
-        return 'Testando';
+        try {
+            return new PDO('pg:host=localhost; dbname=postgres','root','');
+        }catch (PDOException|Exception $e){
+            throw new ErrorException("Erro na conexão com o Postgree");
+        }
     }
 }
+// Principio da inversão de dependencia
+
+class InfoDB {
+    private $dbConnection;
+
+
+    /**
+     * @throws ErrorException
+     */
+    public function __construct(DBConnectionInterface $dbConnection = null) {
+        $defaultConn = new MySQLConnection();
+        $this->dbConnection = $dbConnection ?? $defaultConn;
+        $this->dbConnection = $this->dbConnection->connect();
+    }
+    public function getPdo(): PDO
+    {
+        return $this->dbConnection;
+    }
+}
+
